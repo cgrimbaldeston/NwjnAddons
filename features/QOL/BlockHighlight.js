5272c725ec2508b75179ea2ea38715e6da6efa90
Cameron Lee <127889040+nwjn@users.noreply.github.com>
2025-01-08 18:05:33 -0800

    BlockHighlight: Fix BlockState giving the AABB seizures.

--- features/QOL/BlockHighlight.js
+++ features/QOL/BlockHighlight.js
@@ -2,17 +2,23 @@ import Feature from "../../libs/Features/Feature"
 import RenderUtil from "../../core/static/RenderUtil"
 import Settings from "../../data/Settings"
 
-new Feature({setting: "blockHighlight"})
-    .addEvent(net.minecraftforge.client.event.DrawBlockHighlightEvent, (event) => {
-        const target = event.target
-        if (target?.["typeOfHit", "field_72313_a"]?.toString() !== "BLOCK") return
+const DrawBlockHighlightEvent = net.minecraftforge.client.event.DrawBlockHighlightEvent
+const MovingObjectTypeBLOCK = net.minecraft.util.MovingObjectPosition.MovingObjectType.BLOCK
+const Air = net.minecraft.block.material.Material["air", "field_151579_a"]
 
+new Feature({setting: "blockHighlight"})
+    .addEvent(DrawBlockHighlightEvent, (event) => {
+        const { target } = event
+        if (target["typeOfHit", "field_72313_a"] !== MovingObjectTypeBLOCK) return
+        
         const pos = target["getBlockPos", "func_178782_a"]()
         const WorldClient = World.getWorld()
-        if (!pos || WorldClient["isAirBlock", "func_175623_d"](pos)) return
-
-        const BlockState = WorldClient["getStateAt", "func_180495_p"](pos)["getBlock", "func_177230_c"]()
-        const AABB = BlockState["getSelectedBoundingBox", "func_180646_a"](WorldClient, pos)
+        const BlockAt = WorldClient["getStateAt", "func_180495_p"](pos)["getBlock", "func_177230_c"]()
+        
+        if (BlockAt["getMaterial", "func_149688_o"]() === Air) return
+        
+        BlockAt["setBlockBoundsBasedOnState", "func_180654_a"](WorldClient, pos)
+        const AABB = BlockAt["getSelectedBoundingBox", "func_180646_a"](WorldClient, pos)
         
         const color = Settings().highlightColor
         RenderUtil.drawOutlinedAABB(AABB, color[0], color[1], color[2], color[3], false, 3)
