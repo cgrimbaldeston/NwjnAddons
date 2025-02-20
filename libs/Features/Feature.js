7a6284c809ecf07757c2be2549610c6fe07da2aa
Cameron Lee <127889040+nwjn@users.noreply.github.com>
2025-02-19 22:47:08 -0800

    Feature: fix override

--- libs/Features/Feature.js
+++ libs/Features/Feature.js
@@ -10,65 +10,52 @@ import Location from "../../utils/Location"
 import Event from "../Events/Event";
 
 export default class Feature {
-    /** @override Listener function that's called when this [Feature] is registered */ onRegister;
-    /** @override Listener function that's called when this [Feature] is registered */ onUnregister;
+    /** @override Listener function that's called when this [Feature] is registered */ onRegister() {}
+    /** @override Listener function that's called when this [Feature] is unregistered */ onUnregister() {}
 
     /**
      * - Utility that handles registering various events and listeners to make complex, functional, and performative features
      * - Class can be used with or without requiring the settings, worlds, or zones fields depending on the intended functionality
      * 
      * @param {Object} obj
-     * @param {String} obj.setting The main config name: If null -> Feature is always active, If setting returns false -> all events of this feature will be unregistered
-     * @param {String[]|String} obj.worlds The world(s) where this feature should activate: If null -> Feature is not world dependent
-     * @param {String[]|String} obj.zones The zones(s) where this feature should activate: If null -> Feature is not zone dependent
+     * @param {String|null} obj.setting The main config name: If null -> Feature is always active, If setting returns false -> all events of this feature will be unregistered
+     * @param {String[]|String|null} obj.worlds The world(s) where this feature should activate: If null -> Feature is not world dependent
+     * @param {String[]|String|null} obj.zones The zones(s) where this feature should activate: If null -> Feature is not zone dependent
      */
-    constructor({
-        setting = null,
-        worlds = null,
-        zones = null
-    } = {}) {
-        // Feature should listen for setting changes if required
+    constructor({setting = null, worlds = null, zones = null} = {}) {
+        if (worlds) this.worlds = worlds
+        if (zones) this.zones = zones
+        // Main setting enables/disables entire [Feature]
         if (setting in Settings()) {
-            this.setting = setting
-            this.settingValue = Settings()[setting]
+            this.isSettingEnabled = Settings()[setting]
     
-            Settings().getConfig().registerListener(setting, (_, val) => {
-                this.settingValue = val
-                this._updateRegister()
-            })
+            Settings().getConfig().registerListener(setting, (_, val) => this._updateRegister(this.isSettingEnabled = val))
         }
 
-        // Feature should be updated when the area changes
-        if (worlds) this.worlds = Array.isArray(worlds) ? worlds : Array(worlds)
+        // Will always update on world changes
+        this._updateRegister()
         Location.registerWorldChange(() => this._updateRegister())
-
-        if (zones) {
-            this.zones = Array.isArray(zones) ? zones : Array(zones)
-            Location.registerWorldChange(() => this._updateRegister())
-        }
+        if (zones) Location.registerWorldChange(() => this._updateRegister())
     }
 
     /**
-     * - Adds an [Event] that is registered as long as the Feature is
-     * @param {String|com.chattriggers.ctjs.triggers.Trigger|net.minecraftforge.fml.common.eventhandler.Event} triggerType
-     * @param {Function} methodFn
-     * @param {any} args
-     * @returns {this} meth chain
+     * - Runs the condition function when [Feature] is update and registers if condition passes
+     * - Inits subEvent dependencies if called for the first time
      */
     addEvent(triggerType, methodFn, args) {
-        if (!("events" in this)) this.events = []
+        if (!("events" in this)) {
+            this.events = []
+            this.registerEvents = () => this.events.forEach(event => event.register())
+            this.unregisterEvents = () => this.events.forEach(event => event.unregister())
+        }
         this.events.push(new Event(triggerType, methodFn, args, false))
 
         return this
     }
 
     /**
-     * - Adds a (conditional Event) aka [SubEvent] that is (un)registered by the main events
-     * @param {String} triggerType
-     * @param {(...args) => void} methodFn
-     * @param {any} args
-     * @param {() => Boolean} condition The function to check if this subEvent should be (un)registered
-     * @returns {this} meth chain
+     * - Runs the condition function when [Feature] is update and registers if condition passes
+     * - Inits subEvent dependencies if called for the first time
      */
     addSubEvent(triggerType, methodFn, args, condition = () => true) {
         if (!("subEvents" in this)) {
@@ -76,50 +63,37 @@ export default class Feature {
             this.updateSubEvents = () => this.subEvents.forEach(([subEvent, condition]) => condition() ? subEvent.register() : subEvent.unregister())
             this.unregisterSubEvents = () => this.subEvents.forEach(([subEvent]) => subEvent.unregister())
         }
-
         this.subEvents.push([new Event(triggerType, methodFn, args, false), condition])
+
         return this
     }
 
     /**
-     * - Updates the Feature based on the setting, world, and zone criteria
-     * @note Location#inWorld and Location#inZone return true if param is nullish
-     * @returns {this} meth chain
+     * - Updates registers based on setting, world, and zone criteria of this [Feature]
+     * - Location#inWorld and Location#inZone return true if param is nullish
      */
     _updateRegister() {
-        if (this.setting && !this.settingValue) return this._unregister()
+        if (("settingValue" in this) && !this.settingValue) return this._unregister()
         if (!(Location.inWorld(this.worlds) && Location.inZone(this.zones))) return this._unregister()
         
         return this._register()
     }
 
-    /**
-     * - UnRegisters all strung [Events] including subEvents
-     * @returns {this} meth chain
-     */
+    /** UnRegisters all strung [Events] including [SubEvents] */
     _unregister() {
-        if (!this.isRegistered) return this
-        this.isRegistered = false
+        if (!this.isRegistered) return this.isRegistered = false
         
-        this.events?.forEach(event => event.unregister())
+        this.unregisterEvents?.()
         this.unregisterSubEvents?.()
-        this.onUnregister?.()
-    
-        return this
+        this.onUnregister()
     }
 
-    /**
-     * - UnRegisters all strung [Events] including subEvents
-     * @returns {this} meth chain
-     */
+    /** Registers all strung [Events] and updates [SubEvents] */
     _register() {
-        if (this.isRegistered) return this
-        this.isRegistered = true
+        if (this.isRegistered) return this.isRegistered = true
         
-        this.events?.forEach(event => event.register())
+        this.registerEvents?.()
         this.updateSubEvents?.()
-        this.onRegister?.()
-    
-        return this
+        this.onRegister()
     }
 }
