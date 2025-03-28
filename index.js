efab15249d3e42f564ae14e124d4c48176bb3f1c
Cameron Lee <127889040+nwjn@users.noreply.github.com>
2025-03-27 22:38:40 -0700

    Loader: void invocation, remove some overhead on stack trace

--- index.js
+++ index.js
@@ -7,15 +7,22 @@ import "./utils/Profile"
 import "./libs/Time/ServerTime"
 
 // Handles loading all Feature files because I was too lazy to type them all out
-const pathFinder = /NwjnAddons[\/\\]features[\/\\](.+[\/\\]\w+)\.js$/;
-const fileSeparator = /\\/g;
-const relativeDest = "./features/";
+const pathFinder = /NwjnAddons[\/\\]features[\/\\](.+[\/\\]\w+)\.js$/
+const fileSeparator = /\\/g
+const relativeDest = "./features/"
 
-(function requireFeatures(file) {
-    if (file.isDirectory()) return file.listFiles().forEach(file => requireFeatures(file));
+const modules = []
+
+void function requireFeatures(file) {
+    if (file.isDirectory()) return file.listFiles().forEach(file => requireFeatures(file))
         
-    const match = file.getPath().match(pathFinder);
-    if (!match) return;
+    const match = file.getPath().match(pathFinder)
+    if (!match) return
     
-    require(relativeDest + match[1].replace(fileSeparator, "/"));
-})(new java.io.File(`${Config.modulesFolder}/NwjnAddons/features`));
+    modules.push(
+        relativeDest + match[1].replace(fileSeparator, "/")
+    )
+}(new java.io.File(`${Config.modulesFolder}/NwjnAddons/features`))
+
+let node
+while (node = modules.pop()) require(node)
